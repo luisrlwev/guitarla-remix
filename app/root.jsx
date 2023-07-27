fafee3bfa49f41cd1230f84eaec93af4e1187783
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-27 17:53:19 -0500

    Se agregaron nuestras paginas nuevas al routing - clase 194

--- app/root.jsx
+++ app/root.jsx
@@ -1,33 +1,63 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
+import{
+    Meta,
+    Links,
+    Outlet
+} from '@remix-run/react'
+import styles from './styles/index.css'
 
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+export function meta() {
+    return [
+        {charset: 'utf-8'},
+        {title: 'GuitarLA - Remix'},
+        {viewport: 'width=device-width,initial-scale=1'}
+    ]
+}
 
-export const links = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+export function links() {
+    return [
+        {
+            rel: 'stylesheet',
+            href: 'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com'
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com',
+            crossOrigin: 'true'
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap'
+        },
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
 
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+export default function App(){
+
+    return(
+        <Document>
+            <Outlet/>
+        </Document>
+    )
+}
+
+function Document({children}){
+    return (
+        <html lang="es">
+        <head>
+            <Meta/>
+            <Links/>
+        </head>
+        <body>
+            {children}
+        </body>
+        </html>
+    )
 }
