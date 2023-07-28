19d151fe1ffccb0763876c8462d4fedbb585afb3
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-28 17:57:00 -0500

    Creamos el footer y creamos el componente de navegacion - clase 201

--- app/root.jsx
+++ app/root.jsx
@@ -7,6 +7,7 @@ import{
 } from '@remix-run/react'
 import styles from '~/styles/index.css'
 import Header from '~/components/header'
+import Footer from '~/components/footer'
 
 export function meta() {
     return [
@@ -61,7 +62,7 @@ function Document({children}){
         <body>
             <Header/>
             {children}
-
+            <Footer/>
             <Scripts/>
             <LiveReload/>
         </body>
