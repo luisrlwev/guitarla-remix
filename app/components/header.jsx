5f2f1485283d94159079f1d40e729889950aed64
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-28 16:30:49 -0500

    Agregamos el logo al proyecto - clase 197

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "@remix-run/react"
+import logo from '../../public/img/logo.svg'
 
 function Header() {
   return (
     <header className="header">
         <div className="contenedor barra">
-            <div className="logo">
-
-            </div>
+            <Link to="/">
+                <img src={logo} alt="Logo" className="logo" />
+            </Link>
             <nav className="navegacion">
                 <Link
                     to="/"
