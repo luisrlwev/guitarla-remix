7b7dd59ef8975956cef7257f9d4f9fa7fc840381
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-31 17:58:11 -0500

    Añadimos las guitarras a la tienda a través de nuestra API de Strappi - clase 207

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,6 +1,31 @@
+import { useLoaderData } from '@remix-run/react'
+import { getGuitarras } from "~/models/guitarras.server"
+import Guitarra from '~/components/guitarra'
+
+export async function loader(){
+  const guitarras = await getGuitarras()
+  return guitarras.data
+}
+
 function Tienda() {
+
+  const guitarras = useLoaderData()
+
   return (
-    <div>tienda</div>
+    <main className='contenedor'>
+      <h2 className='heading'>Nuestra Coleccion</h2>
+
+      {guitarras?.length && (
+        <div className="guitarras-grid">
+          {guitarras.map( guitarra => (
+            <Guitarra
+            key={guitarra?.id}
+            guitarra={guitarra?.attributes}
+            />
+          ))}
+        </div>
+      )}
+    </main>
   )
 }
 
