9c3476996c48543795050ebc7aa115e103bdb077
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-07 17:42:27 -0500

    formateamos la fecha del blog y correjimos algunos estilos

--- app/components/post.jsx
+++ app/components/post.jsx
@@ -1,4 +1,6 @@
 import { Link } from "@remix-run/react"
+import { formatearFecha } from "~/utils/helpers"
+
 export default function Post({post}) {
 
     const { contenido, imagen, titulo, url, publishedAt } = post
@@ -7,7 +9,7 @@ export default function Post({post}) {
         <img src={imagen.data.attributes.formats.small.url} alt={`Imagen del blog ${titulo}`} className="imagen"/>
         <div className="contenido">
             <h3>{titulo}</h3>
-            <p className="fecha">{publishedAt}</p>
+            <p className="fecha">{formatearFecha(publishedAt)}</p>
             <p className="resumen">{contenido}</p>
             <Link className="enlace" to={`/posts/${url}`}>Leer más</Link>
         </div>
