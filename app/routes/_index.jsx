1ec7ce2a363e6f6b3be2d43d2cba3c6742497902
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-09 17:55:56 -0500

    Hicimos multiple llamados a las Apis y además listamos las guitarras en el index

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,8 +1,47 @@
+import { useLoaderData } from '@remix-run/react'
+import { getGuitarras } from '~/models/guitarras.server'
+import { getPosts } from '~/models/posts.server'
+import ListadoGuitarras from '~/components/listado-guitarras'
+import stylesGuitarras from '~/styles/guitarras.css'
+
+export function meta(){
+
+}
+
+export function links(){
+  return[
+    {
+      rel: 'stylesheet',
+      href: stylesGuitarras
+    }
+  ]
+}
+
+export async function loader(){
+
+  const [guitarras, posts] = await Promise.all([
+    getGuitarras(),
+    getPosts()
+  ])
+  return {
+    guitarras: guitarras.data,
+    posts: posts.data
+  }
+}
+
+
 function Index() {
+
+  const {guitarras, posts} = useLoaderData()
+
   return (
-    <div>
-        <h1>Desde index.jsx</h1>
-    </div>
+    <>
+      <main className='contenedor'>
+        <ListadoGuitarras
+          guitarras={guitarras}
+        />
+      </main>
+    </>
   )
 }
 
