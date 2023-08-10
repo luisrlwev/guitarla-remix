ba2fbc2406333b66618c9618fc30d902955eac6b
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-10 15:52:03 -0500

    Se agregó el listado de post al index

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -2,10 +2,15 @@ import { useLoaderData } from '@remix-run/react'
 import { getGuitarras } from '~/models/guitarras.server'
 import { getPosts } from '~/models/posts.server'
 import ListadoGuitarras from '~/components/listado-guitarras'
+import ListadoPosts from '../components/listado-posts'
 import stylesGuitarras from '~/styles/guitarras.css'
+import stylesPosts from '~/styles/blog.css'
 
 export function meta(){
-
+  return[
+    {title: 'Inicio - GuitarLA'},
+    {description: 'Bienvenido a GuitarLA descubre la colección de nuestras guitarras'}
+  ]
 }
 
 export function links(){
@@ -13,6 +18,10 @@ export function links(){
     {
       rel: 'stylesheet',
       href: stylesGuitarras
+    },
+    {
+      rel: 'stylesheet',
+      href: stylesPosts
     }
   ]
 }
@@ -41,6 +50,11 @@ function Index() {
           guitarras={guitarras}
         />
       </main>
+      <section className='contenedor'>
+        <ListadoPosts
+          posts={posts}
+        />
+      </section>
     </>
   )
 }
