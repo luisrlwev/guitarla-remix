63de9e048ce146c98cab207dcc2fae4217b35799
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-14 17:09:35 -0500

    Se creo la hoja de estilos para carrito y se añadieron estilos

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,8 +1,35 @@
+import styles from '~/styles/carrito.css'
 
+export function meta(){
+    return[
+        {title: 'Carrito - GuitarLA'},
+        {description: 'Termina de comprar lo que añadiste al carrito'}
+    ]
+}
+
+export function links(){
+    return[
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
 
 function Carrito() {
   return (
-    <div>Carrito</div>
+    <main className="contenedor">
+        <h1 className="heading">Carrito de compras</h1>
+        <div className="contenido">
+            <div className='carrito'>
+                <h2>Artículos</h2>
+            </div>
+            <aside className="resumen">
+                <h3>Resumen del pedido</h3>
+                <p>Total a pagar: $</p>
+            </aside>
+        </div>
+    </main>
   )
 }
 
