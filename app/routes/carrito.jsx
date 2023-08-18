2dc76e75415e0a57f1b71c80a1657a1bdbf6ebce
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-18 17:28:45 -0500

    Calculamos el total

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { useOutletContext } from '@remix-run/react'
 import styles from '~/styles/carrito.css'
 
@@ -18,7 +19,13 @@ export function links(){
 }
 
 function Carrito() {
+    const [total, setTotal] =  useState(0)
     const { carrito, actualizarCantidad } = useOutletContext()
+
+    useEffect(() => {
+        const calculoTotal = carrito.reduce( (total, producto) => total + (producto.cantidad * producto.precio), 0)
+        setTotal(calculoTotal)
+    }, [carrito])
 
   return (
     <main className="contenedor">
@@ -58,7 +65,7 @@ function Carrito() {
             </div>
             <aside className="resumen">
                 <h3>Resumen del pedido</h3>
-                <p>Total a pagar: $</p>
+                <p>Total a pagar: ${total}</p>
             </aside>
         </div>
     </main>
