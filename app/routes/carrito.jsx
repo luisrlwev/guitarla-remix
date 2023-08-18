4229e1c4736947bfb40c75011d079be33e3aedec
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-18 16:57:58 -0500

    añadimos el select para modificar las cantidades en el carrito

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -33,9 +33,19 @@ function Carrito() {
                             </div>
                             <div>
                                 <p className="nombre">{producto.nombre}</p>
-
-                                <p className='precio'>$ <span>{producto.precio}</span></p>
-                                <p className='subtotal'>Subtotal: $ <span>{producto.cantidad * producto.precio}</span></p>
+                                <p>Cantidad:</p>
+                                <select
+                                    value={producto.cantidad}
+                                    className='select'
+                                >
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select>
+                                <p className='precio'>Precio: $ <span>{producto.precio}</span></p>
+                                <p className='subtotal'>Subtotal: $<span> {producto.cantidad * producto.precio}</span></p>
                             </div>
                         </div>
                     ))
