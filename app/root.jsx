80b8d2308c79b036fbecba8aa4ac97c26bc54916
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-21 17:12:39 -0500

    Añadimos local storage al carrito y resolvimos el problema de hidratacion en Remix - clase 240

--- app/root.jsx
+++ app/root.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import{
     Meta,
     Links,
@@ -50,7 +50,12 @@ export function links() {
 
 export default function App(){
 
-    const [carrito, setCarrito] = useState([])
+    const carritoLS = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('carrito')) ?? [] : null
+    const [carrito, setCarrito] = useState(carritoLS)
+
+    useEffect(() => {
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+    }, [carrito])
 
     const agregarCarrito = guitarra => {
         // Comprobar si el elemento en el carrito ya existe
